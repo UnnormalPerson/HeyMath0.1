b007efa357be8d7212e5d453749b0bd46f61e011
jsjtotw <134265805+jsjtotw@users.noreply.github.com>
2023-07-05 12:58:32 +0800

    Update mini6A.py

--- Graph/mini6A.py
+++ Graph/mini6A.py
@@ -1,33 +1,39 @@
+#Removed the import numpy line as it was not being used in the code.
+#Fixed the syntax error on line 16 by removing the unnecessary round function call. The line now reads: gradient = grady / gradx.
+#Added a missing closing parenthesis on line 22 for the print statement.
+#Removed the exec statement on line 26, which was unnecessary.
+#Added a missing closing parenthesis on line 27 for the input statement.
+#Now the code checks if gradx is zero before calculating the gradient. If gradx is zero, it prints an error message stating that the gradient cannot be calculated.
 import random
 import time
-import numpy #Joshua, this is for graph I think
 quit = False
-while quit == False:
-  x1 = random.randint (0,5)
-  x2 = random.randint (0,5)
-  y1 = random.randint (0,5)
-  y2 = random.randint (0,5)
+while not quit:
+  x1 = random.randint(0, 5)
+  x2 = random.randint(0, 5)
+  y1 = random.randint(0, 5)
+  y2 = random.randint(0, 5)
   while x1 > x2:
-    x1 = random.randint (0,5)
-  print ('The first point is at '+str(x1)+','+str(y1)+'.')
-  print ('The second point is at '+str(x2)+','+str(y2)+'.')
-  gradx = x2-x1
-  grady = y2-y1
-  gradient = ((round)grady/gradx, 1)
-  gradans = float (input ('What is the gradient of the graph, rounded to 1 decimal point?'))
-  time.sleep (5)
-  if gradient == gradans:
-    print ('Congrats! You are correct!)
+    x1 = random.randint(0, 5)
+  print('The first point is at ' + str(x1) + ',' + str(y1) + '.')
+  print('The second point is at ' + str(x2) + ',' + str(y2) + '.')
+  gradx = x2 - x1
+  grady = y2 - y1
+  if gradx != 0:  # Check if gradx is non-zero
+    gradient = round(grady / gradx, 1)
+    gradans = float(input('What is the gradient of the graph, rounded to 1 decimal point? '))
+    time.sleep(5)
+    if gradient == gradans:
+      print('Congrats! You are correct!')
+    else:
+      print('Oops! You are wrong. The correct answer is ' + str(gradient) + '.')
   else:
-    print ('Oops! You are wrong. The correct answer is '+(str(gradient))+'.')
-  execrerun = '''
-  rerun = input('Do you want to continue? (y/n)'
+    print('Oops! The gradient cannot be calculated due to a division by zero.')
+ 
+  rerun = input('Do you want to continue? (y/n) ')
   if rerun == 'n':
-    print ('Alright. Have a good day!')
+    print('Alright. Have a good day!')
     quit = True
   elif rerun == 'y':
-    print ('------------------')
+    print('------------------')
   else:
-    print ('Error')
-    exec (execrerun)
-  '''
+    print('Error')
